dc9e78079ecaf894df8afea5652fc9e3563689e5
Gabriel <78434712+Gabriel-sec@users.noreply.github.com>
2025-12-21 19:32:29 -0500

    Second iteration to test github integration & pr (#1)

--- sensor-mqtt.py
+++ sensor-mqtt.py
@@ -1,6 +1,7 @@
 import time
 import random
 import paho.mqtt.client as mqtt
+# Testing github integration
 
 def get_virtual_temp():
     # Simulates a temperature between 18.0°C and 26.0°C
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     while True:
         temp = get_virtual_temp()
         # Send data to MQTT broker
-        
+
         print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Sensor ID: SN-1001 | Temperature: {temp}°C\n", flush=True)
 
         time.sleep(5) # Send data every 5 seconds
