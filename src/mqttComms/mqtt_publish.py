c5f5d0c2ccb395f64488c26c9aa1ca34d87bc89b
Gabriel-sec <78434712+Gabriel-sec@users.noreply.github.com>
2026-01-10 18:21:14 -0500

    dev: linking mqtt modules to temperature read.py

--- src/mqttComms/mqtt_publish.py
+++ src/mqttComms/mqtt_publish.py
@@ -1,6 +1,5 @@
 from src.mqttComms.mqtt_connect import MQTTConnector
 import time #This comes pre-installed with Python. No need to define it as a dependency in poetry
-from src.config.settings import BROKER_ADDRESS, BROKER_PORT, TOPIC
 
 class MQTTPublisher(MQTTConnector):
     # Client is created and the hooks are attached before the Publisher starts its own work.
@@ -8,11 +7,11 @@ class MQTTPublisher(MQTTConnector):
         super().__init__(BROKER_ADDRESS, BROKER_PORT)
         self.TOPIC=TOPIC
         
-    def publish(self):
+    def publish(self, msg):
         msg_count = 1
         while True:
             time.sleep(1)
-            msg = f"messages: {msg_count}"
+            #msg = f"messages: {msg_count}"
             result = self.client.publish(self.TOPIC, msg)
             # result: [0, 1]
             status = result[0]
@@ -23,15 +22,10 @@ class MQTTPublisher(MQTTConnector):
             msg_count += 1
             if msg_count > 5:
                 break
-    def start(self):
+    def start(self, msg):
         self.broker_connection()
-        self.publish()
-            
-if __name__ == "__main__":
-    mqtt_publish = MQTTPublisher(BROKER_ADDRESS,BROKER_PORT, TOPIC)
-    mqtt_publish.start()
-    time.sleep(2)
-    mqtt_publish.client.loop_stop()
+        self.publish(msg)
+    
 
     
 
